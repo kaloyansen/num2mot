3436b496728a7798a905baaf26cc20c37a88066d
Kaloyan KRASTEV <83513233+kaloyansen@users.noreply.github.com>
2021-05-09 13:11:51 +0200

    Update num2mot.py

--- num2mot.py
+++ num2mot.py
@@ -94,53 +94,89 @@ class num2mot:
 				j = int(i / 1e2)
 				m = self.tran3(j)
 				i -= 1e2 * j
-				if i == 0 and j > 1: n = n + m + ' cents'
+				if i == 0 and j > 1:
+					n = n + m + ' cents'
 				else:
 					if j > 1:
 						n = n + m + ' cent '
 					else:
 						n = n + 'cent '
-			elif i >  80: i, n = self.tran2(i, n, 'quatre-vingt', 80)
-			elif i == 80: i, n = self.tran2(i, n, 'quatre-vingts', 80)
-			elif i >= 60: i, n = self.tran2(i, n, 'soixante', 60)
-			elif i >= 50: i, n = self.tran2(i, n, 'cinquante', 50)
-			elif i >= 40: i, n = self.tran2(i, n, 'quarante', 40)
-			elif i >= 30: i, n = self.tran2(i, n, 'trente', 30)
-			elif i >= 20: i, n = self.tran2(i, n, 'vingt', 20)
-			elif i >  16: i, n = self.tran2(i, n, 'dix', 10)
-			elif i == 16: i, n = self.tran2(i, n, 'seize', 16)
-			elif i == 15: i, n = self.tran2(i, n, 'quinze', 15)
-			elif i == 14: i, n = self.tran2(i, n, 'quatorze', 14)
-			elif i == 13: i, n = self.tran2(i, n, 'treize', 13)
-			elif i == 12: i, n = self.tran2(i, n, 'douze', 12)
+			elif i >  80:
+				i, n = self.tran2(i, n, 'quatre-vingt', 80)
+			elif i == 80:
+				i, n = self.tran2(i, n, 'quatre-vingts', 80)
+			elif i >= 60:
+				i, n = self.tran2(i, n, 'soixante', 60)
+			elif i >= 50:
+				i, n = self.tran2(i, n, 'cinquante', 50)
+			elif i >= 40:
+				i, n = self.tran2(i, n, 'quarante', 40)
+			elif i >= 30:
+				i, n = self.tran2(i, n, 'trente', 30)
+			elif i >= 20:
+				i, n = self.tran2(i, n, 'vingt', 20)
+			elif i >  16:
+				i, n = self.tran2(i, n, 'dix', 10)
+			elif i == 16:
+				i, n = self.tran2(i, n, 'seize', 16)
+			elif i == 15:
+				i, n = self.tran2(i, n, 'quinze', 15)
+			elif i == 14:
+				i, n = self.tran2(i, n, 'quatorze', 14)
+			elif i == 13:
+				i, n = self.tran2(i, n, 'treize', 13)
+			elif i == 12:
+				i, n = self.tran2(i, n, 'douze', 12)
 			elif i == 11:
-				if n.isspace() or n == '' or n.endswith('cent '): n += 'onze'
-				elif n.endswith('quatre-vingt'): n += '-onze'
-				elif n.endswith('cent') or n.endswith('mille'): n += ' onze'
-				elif n.endswith('million') or n.endswith('milliard'): n += ' onze'
-				elif n.endswith('vingt') or n.endswith('trente'): n += ' et onze'
-				elif n.endswith('quarante') or n.endswith('cinquante') or n.endswith('soixante'): n += ' et onze'
-				else: n += '-onze'
+				if n.isspace() or n == '' or n.endswith('cent '):
+					n += 'onze'
+				elif n.endswith('quatre-vingt'):
+					n += '-onze'
+				elif n.endswith('cent') or n.endswith('mille'):
+					n += ' onze'
+				elif n.endswith('million') or n.endswith('milliard'):
+					n += ' onze'
+				elif n.endswith('vingt') or n.endswith('trente'):
+					n += ' et onze'
+				elif n.endswith('quarante') or n.endswith('cinquante') or n.endswith('soixante'):
+					n += ' et onze'
+				else:
+					n += '-onze'
 				i -= 11
-			elif i == 10: i, n = self.tran2(i, n, 'dix', 10)
-			elif i == 9: i, n = self.tran2(i, n, 'neuf', 9)
-			elif i == 8: i, n = self.tran2(i, n, 'huit', 8)
-			elif i == 7: i, n = self.tran2(i, n, 'sept', 7)
-			elif i == 6: i, n = self.tran2(i, n, 'six', 6)
-			elif i == 5: i, n = self.tran2(i, n, 'cinq', 5)
-			elif i == 4: i, n = self.tran2(i, n, 'quatre', 4)
-			elif i == 3: i, n = self.tran2(i, n, 'trois', 3)
-			elif i == 2: i, n = self.tran2(i, n, 'deux', 2)
+			elif i == 10:
+				i, n = self.tran2(i, n, 'dix', 10)
+			elif i == 9:
+				i, n = self.tran2(i, n, 'neuf', 9)
+			elif i == 8:
+				i, n = self.tran2(i, n, 'huit', 8)
+			elif i == 7:
+				i, n = self.tran2(i, n, 'sept', 7)
+			elif i == 6:
+				i, n = self.tran2(i, n, 'six', 6)
+			elif i == 5:
+				i, n = self.tran2(i, n, 'cinq', 5)
+			elif i == 4:
+				i, n = self.tran2(i, n, 'quatre', 4)
+			elif i == 3:
+				i, n = self.tran2(i, n, 'trois', 3)
+			elif i == 2:
+				i, n = self.tran2(i, n, 'deux', 2)
 			elif i == 1:
-				if n == '' or n.isspace() or n.endswith('cent '): n += 'un'
-				elif n.endswith('quatre-vingt'): n += '-un'
-				elif n.endswith('cent') or n.endswith('mille') or n.endswith('million') or n.endswith('milliard'): n += ' un'
-				elif n.endswith('vingt') or n.endswith('trente') or n.endswith('quarante') or n.endswith('cinquante') or n.endswith('soixante'): n += ' et un'
-				else: n += '-un'
+				if n == '' or n.isspace() or n.endswith('cent '):
+					n += 'un'
+				elif n.endswith('quatre-vingt'):
+					n += '-un'
+				elif n.endswith('cent') or n.endswith('mille') or n.endswith('million') or n.endswith('milliard'):
+					n += ' un'
+				elif n.endswith('vingt') or n.endswith('trente') or n.endswith('quarante') or n.endswith('cinquante') or n.endswith('soixante'):
+					n += ' et un'
+				else:
+					n += '-un'
 				i -= 1
 			else:
 				print ('ça ne sera pas')
-				if n.isspace(): n += 'zéro'
+				if n.isspace():
+					n += 'zéro'
 
 		return n
 
@@ -148,7 +184,9 @@ class num2mot:
 	def tran4(self, i, maxi, t1='', t2=''):
 		j = int(i / maxi)
 		i = i - maxi * j
-		if j > 1: self.mot += self.tran3(j) + t2
-		else: self.mot += t1
+		if j > 1:
+			self.mot += self.tran3(j) + t2
+		else:
+			self.mot += t1
 		return i
 
